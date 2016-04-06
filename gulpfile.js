0abaf0bc9b7bb0e04a175bb4f69676ac5a00b09c
Damien Barchowsky <dbarchowsky@gmail.com>
2016-04-05 17:18:38 -0700

    Adds gulp-load-plugins #5

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,6 @@
 var gulp = require('gulp');
-var karma = require('karma');
-var jasmine = require('gulp-jasmine');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var banner = require('gulp-banner');
+var $ = require('gulp-load-plugins')();
+var Server = require('karma').Server;
 var runSequence = require('run-sequence');
 var pkg = require("./package.json");
 
@@ -20,37 +14,37 @@ var comment = "/*\n" +
     " */\n";
 
 gulp.task('tests', function(done) {
-   return karma.start({
+   return new Server({
        configFile: __dirname + '/karma.conf.js',
-       singleRun: false
-   }, done); 
+       singleRun: true
+   }, done).start();
 });
 
 gulp.task('jshint', function() {
    return gulp.src(['src/*.js', 'test/**/*.js'])
-       .pipe(jshint(".jshintrc"))
-       .pipe(jshint.reporter('default'));
+       .pipe($.jshint(".jshintrc"))
+       .pipe($.jshint.reporter('default'));
 });
 
 gulp.task('jscs', function() {
    return gulp.src('src/**/*.js')
-       .pipe(jscs())
-       .pipe(jscs.reporter());
+       .pipe($.jscs())
+       .pipe($.jscs.reporter());
 });
 
 gulp.task('concat', function() {
    return gulp.src('./src/jquery.lightbox.js')
-       .pipe(banner(comment, { pkg: pkg }))
-       .pipe(concat('jquery.lightbox.js'))
+       .pipe($.banner(comment, { pkg: pkg }))
+       .pipe($.concat('jquery.lightbox.js'))
        .pipe(gulp.dest('./dist/'))
        .pipe(gulp.dest('./demo/public/js/'));
 });
 
 gulp.task('uglify', function() {
    return gulp.src('./dist/jquery.lightbox.js')
-       .pipe(rename('jquery.lightbox.min.js'))
-       .pipe(uglify())
-       .pipe(banner(comment, { pkg: pkg }))
+       .pipe($.rename('jquery.lightbox.min.js'))
+       .pipe($.uglify())
+       .pipe($.banner(comment, { pkg: pkg }))
        .pipe(gulp.dest('./dist/'))
        .pipe(gulp.dest('./demo/public/js/'));
 });
